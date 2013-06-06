d3aef4dec75e2de898ee143a9e9f9ebe46cd51c6
元彦 <yuanyan.cao@gmail.com>
2013-06-06 15:08:40 +0800

    readme

---
+++ README.md
@@ -0,0 +1,18 @@
+## jQuery sonar effect plugin
+
+### Usage
+
+```html
+<link href="../sonar.css" rel="stylesheet">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script src="../sonar.js"></script>
+```
+
+```js
+$('.icon').sonar();
+```
+
+### Demo
+![sonar](https://f.cloud.github.com/assets/677114/616669/c276b276-ce77-11e2-8b99-64fe08e6e9fa.gif)
+
+
--- sonar.js
+++ sonar.js
@@ -10,8 +10,26 @@
 
     $.fn[pluginName] = function(options){
         return this.each(function () {
-            // ...
+            var $this = $(this);
+            var pos = $this.position();
+            var styles = {
+                position: 'absolute',
+                top: pos.top,
+                left: pos.left,
+                width: $this.width(),
+                height: $this.height(),
+                borderRadius: $this.css('border-radius'), // border-radius
+                zIndex: $this.css('z-index')-1, // z-index
+                animation: 'sonarEffect 1.3s ease-out 75ms'
+            };
 
+            var $sonarEffect = $('<span></span>').css(styles);
+
+            $this.hover(function(){
+                $this.after($sonarEffect);
+            }, function(){
+                $sonarEffect.remove();
+            })
 
         })
     };
